abbbe4ac6bc79a38b14130f499707dfc5d898028
Sowmiya <sowmiya.a@praniontech.com>
2026-02-13 11:43:52 +0530

    vm

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Chat Application-v1';
+    return 'Chat Application-version1';
   }
 }
